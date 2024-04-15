d7f38195addf3b6a3ae580037ce936b8ac0f4f78
vikas-singh-7 <vs7597186@gmail.com>
2024-04-15 21:30:44 +0530

    detailsBoxdone

--- src/components/DetailBox.jsx
+++ src/components/DetailBox.jsx
@@ -3,26 +3,32 @@ import DetailCompo from "./DetailCompo";
 
 const DetailBox = () => {
   return (
-    <div className="bg-blue-500 h-full px-28 py-8">
-      <div className="h-auto w-full bg-yellow-500 grid grid-cols-12 grid-rows-12">
-        <div className="row-span-3 col-span-6 bg-green-500 p-2">
+    <div className=" h-auto w-[100vw] px-28 py-8">
+      <div className="h-auto w-full  grid grid-cols-12 grid-rows-12 gap-4">
+        <div
+          className="row-span-4 col-span-6 hover:cursor-pointer hover:scale-[1.02] transition-all ease-out duration-75 p-2"
+          
+        >
           <DetailCompo />
         </div>
-        <div className="row-span-3 col-span-6 bg-green-500 p-2">
+        <div className="row-span-4 col-span-6 hover:cursor-pointer p-2 hover:scale-[1.02] transition-all ease-in-out duration-100 ">
           <DetailCompo />
         </div>
-        <div className="row-span-3 col-span-6 bg-green-500 p-2">
+        <div className="row-span-4 col-span-6 hover:cursor-pointer p-2 hover:scale-[1.02] transition-all ease-in-out duration-100">
           <DetailCompo />
         </div>
-        <div className="row-span-3 col-span-6 bg-green-500 p-2">
+        <div className="row-span-4 col-span-6 hover:cursor-pointer p-2 hover:scale-[1.02] transition-all ease-in-out duration-100">
           <DetailCompo />
         </div>
-        <div className="row-span-3 col-span-6 bg-green-500 p-2">
+        <div className="row-span-4 col-span-6 hover:cursor-pointer p-2 hover:scale-[1.02] transition-all ease-in-out duration-100">
           <DetailCompo />
         </div>
-        <div className="row-span-3 col-span-6 bg-green-500 p-2">
+        <div className="row-span-4 col-span-6 hover:cursor-pointer p-2 hover:scale-[1.02] transition-all ease-in-out duration-100">
           <DetailCompo />
         </div>
+      </div>
+      <div className="h-[10vh] w-full flex justify-center items-center">
+        <button className="rounded-md bg-[#2D85F3] mt-5 text-white font-semibold px-3 py-2">Core Pillars</button>
       </div>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
    
          <div className='md:h-[80vh] max-sm:h-[50vh] w-[100vw] bg-[url("./images/heroImage.webp")]  bg-center bg-cover relative max-sm:bg-[url("./images/heromobile.webp")]'>
         <div className="absolute left-[50%] top-[50%] -translate-y-[50%] -translate-x-[50%] p-2 w-[40%] flex justify-center flex-col items-center ">
-          <h1 className="text-[4rem] font-mono leading-none tracking-tight  font-semibold">
+          <h1 className="text-[4rem]  leading-none tracking-tight  font-semibold font-mono">
             Apna Himachal
           </h1>
           <p className="mt-4 text-[1.5rem] font-serif font-light">
