064e5ff91d4fde8876a9d1339a1a13263e7ed27a
vikas-singh-7 <vs7597186@gmail.com>
2024-04-17 16:49:39 +0530

    DoingAboutPage

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,29 +1,37 @@
 import React from "react";
 import Navbar from "./Navbar";
-import Pillars from "./Pillars";
 import LogChild from "./LogChild";
 
 const About = () => {
   const details = [
     {
-      logo:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fbrandslogos.com%2Fr%2Freact-logo-1%2F&psig=AOvVaw10xcIyVlUWAgVl8b2QHiJk&ust=1713437430038000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMj6tv6JyYUDFQAAAAAdAAAAABAE",
+      logo: "./images/pekka.webp",
       details: [
         "HTML",
         "Css",
         "ReactJs",
         "TailwindCss",
-        "React Router",
+        "React Router Dom",
         "React Icons",
-        "Locomotive Scroll"
+        "Vite",
+        "Locomotive Scroll",
       ],
-      image: "",
+      image: "./images/tech1.png",
     },
   ];
   return (
     <>
       <Navbar />
-      {/* <LogChild  /> */}
+      {details.map((item) => {
+        return (
+          <LogChild
+            logo={item.logo}
+            image={item.image}
+            details={item.details}
+          />
+        );
+      })}
+      <div className="h-[70vh] bg-yellow-500"></div>
     </>
   );
 };
--- src/components/LogChild.jsx
+++ src/components/LogChild.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-const LogChild = ({ image, details, logo }) => {
+const  LogChild = ({ image, details, logo }) => {
   return (
     <div className="h-auto">
       <div className="h-[100vh] bg-[#1F7AEB] grid gap-2 grid-cols-2 grid-rows-1 p-5">
         <div
-          className="  bg-cover bg-center col-span-1 bg-[url('./images/logs.png')]"
+          className="  bg-cover bg-center col-span-1 bg-[url('./images/logs.png')] rounded-md"
           style={{ backgroundImage: `url(${logo})` }}
         ></div>
         <div className="col-span-1 border-2  gap-5 rounded-md grid p-2 grid-cols-1 grid-rows-2">
           <div className="row-span-1">
-            <img className="rounded-md" src={image} alt="" />
+            <img className="rounded-md" src={image} alt=""  />
           </div>
           <div className="row-span-1 text-white font-semibold font-mono px-5 flex justify-center items-center ">
             <div className="h-full w-full ">
